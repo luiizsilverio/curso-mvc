f7e22ca199c25aa2027ff456a382bf69a291a359
luiizsilverio <luiiz.silverio@gmail.com>
2024-08-08 14:00:57 -0300

    Ajuste final

--- index.php
+++ index.php
@@ -6,20 +6,4 @@
 
   $router = new Router;
 
-
-
-
 ?>
-
-<!-- 
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Curso PHP MVC</title>
-</head>
-<body>
-  <h1>MVC</h1>
-</body>
-</html> -->
